bb9e114ee94678f470dbe3dd058027851eaec739
Rohan Sharma <rohan.sharma@research.iiit.ac.in>
2020-04-04 17:16:01 +0530

    Fixed the error in the database relation

--- hmrm/models.py
+++ hmrm/models.py
@@ -33,6 +33,7 @@ class Users(db.Model):
             'email': self.email,
         }
 
+
 class Hospital(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
@@ -45,12 +46,16 @@ class Hospital(db.Model):
     num_testing_kits = db.Column(db.Integer)
     testing_capacity = db.Column(db.Integer)
 
+
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     sex = db.Column(db.String(1))
-    hospital_id = db.Column(db.Integer, db.ForeignKey(Hospital.id, ondelete="CASCADE"), nullable=False)
-    hospital_ref = db.relationship("hospital", backref=db.backref("hospital_from_patient", cascade="all"))
+    hospital_id = db.Column(db.Integer, db.ForeignKey(
+        Hospital.id, ondelete="CASCADE"), nullable=False)
+    # hospital_ref = db.relationship("hospital", backref=db.backref("hospital_from_patient", cascade="all"))
+    hospital_ref = db.relationship(
+        "Hospital", backref=db.backref("Hospital", cascade="all"))
 
     condition = db.Column(db.String(80))
     age = db.Column(db.Integer)
